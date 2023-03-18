e77f4bffa2fa4594e655c898b1265c93cda6622c
Stanislav Korolkov <stasevich741@gmail.com>
2023-03-18 17:51:13 +0300

    assertions

--- src/test/java/com/example/googletestapi/FirstTest.java
+++ src/test/java/com/example/googletestapi/FirstTest.java
@@ -2,15 +2,30 @@ package com.example.googletestapi;
 
 import com.example.googletestapi.config.TestConfig;
 import com.example.googletestapi.constant.TestConstant;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class FirstTest extends TestConfig {
     @Test
     public void myFirstTest() {
-        given().log().uri()
-                .when().get(TestConstant.Action.SWAPI_GET_PEOPLE + "1")
-                .then().log().body().statusCode(200);
+        given().spec(requestSpecificationSwapiTest).log().uri()
+                .when().get(TestConstant.Path.SWAPI_PATH)
+                .then().body("people", equalTo("https://swapi.dev/api/people/")).log().body();
+    }
+
+    @Test
+    @DisplayName("get some field is response with index assertion test")
+    void getSomeFieldIsResponseWithIndexAssertionTest() {
+        given().spec(requestSpecificationSwapiTest).log().uri()
+                .when().get(TestConstant.Path.SWAPI_PATH + TestConstant.Action.SWAPI_GET_PEOPLE)
+                .then()
+                .body("count", equalTo(82))
+                .body("results.name[0]", equalTo("Luke Skywalker"))
+                .body("results.mass[0]", equalTo("77"))
+                .body("results.name[1]", equalTo("C-3PO"))
+                .log().body();
     }
 }
--- src/test/java/com/example/googletestapi/config/TestConfig.java
+++ src/test/java/com/example/googletestapi/config/TestConfig.java
@@ -3,10 +3,16 @@ package com.example.googletestapi.config;
 import com.example.googletestapi.constant.TestConstant;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestConfig {
+
+    protected static RequestSpecification requestSpecificationSwapiTest = new RequestSpecBuilder()
+            .setBaseUri(TestConstant.Server.SWAPI_URL)
+            .build();
 
     protected static RequestSpecification requestSpecificationJson = new RequestSpecBuilder()
             .addHeader("Content-Type", "application/json")
@@ -18,6 +24,14 @@ public class TestConfig {
             .addHeader("Content-Type", "application/xml")
             .addCookie("testCookieXml")
             .setBaseUri(TestConstant.Server.REQUESTBIN_URL)
+            .build();
+
+    protected static ResponseSpecification responseSpecificationForGet = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .build();
+
+    protected static ResponseSpecification responseSpecificationForPost = new ResponseSpecBuilder()
+            .expectStatusCode(201)
             .build();
 
     @BeforeAll
